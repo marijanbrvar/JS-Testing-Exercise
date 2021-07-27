a0a6db6256fd4a95cedfd59ec142e400cbad180f
Marijan Brvar <marijan.brvar@gmail.com>
2021-07-27 12:41:21 +0200

    Add 2nd exercise

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -7,3 +7,12 @@ test('Takes any string as an argument and returns its characters count', () => {
 test('Takes any string as an argument and returns its characters count', () => {
   expect(functions.stringLength('marijan')).not.toBe(8);
 });
+
+test('Check if string is to short or too long then return error', () => {
+  expect(functions.stringLength('m')).toBe('String is not long enough or it is too long, more than 10 char!');
+  expect(functions.stringLength('123456789011')).toBe('String is not long enough or it is too long, more than 10 char!');
+});
+
+test('Reverse string provided as argument', () => {
+  expect(functions.reverseString('1234567890')).toBe('0987654321');
+});
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const functions = {
     if ((string.length <= 1) || (string.length > 10)) return 'String is not long enough or it is too long, more than 10 char!';
     return string.length;
   },
+  reverseString: (string) => string.split('').reverse().join(''),
 };
 
 module.exports = functions;
